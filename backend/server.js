1c494397cef56b29e23142f3ee48c414cb6c1359
mohammed chaib <mohamedchaib964@gmail.com>
2025-05-22 23:53:10 +0100

    hh

--- backend/server.js
+++ backend/server.js
@@ -36,8 +36,7 @@ dotenv.config()
 const app = express() 
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors());
-app.use(cors({
+app.use(cors('*',{
   origin: 'http://localhost:3000', // allow all
   credentials:true
 }));

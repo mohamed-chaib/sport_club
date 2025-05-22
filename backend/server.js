032bab0ded58f411a3080beb28ec64f42ec31f88
mohammed chaib <mohamedchaib964@gmail.com>
2025-05-23 00:26:06 +0100

    helo

--- backend/server.js
+++ backend/server.js
@@ -36,12 +36,18 @@ dotenv.config()
 const app = express() 
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors('*',{
-  origin: 'http://localhost:3000', // allow all
-  credentials:true,
-  methods:['GET','POST','DELETE','OPTIONS','PUT','PATCH'],
-  allowHeaders:['Content-Type','Authorization']
-}));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); 
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200); // استجابة سريعة لطلبات preflight
+  }
+
+  next();
+});
 
 // set up associations 
  setupAssociations()

b4de24d5b80a92370858a47de4966da641d1844b
mohammed chaib <mohamedchaib964@gmail.com>
2025-05-22 23:33:10 +0100

    allwo inly ne site

--- backend/server.js
+++ backend/server.js
@@ -38,7 +38,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(cors());
 app.use(cors({
-  origin: '*', // allow all
+  origin: 'http://localhost:3000', // allow all
   credentials:true
 }));
 
